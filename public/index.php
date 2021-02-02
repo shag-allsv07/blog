6284e14d18f1694d4209c337436b50112c13f745
Your Name <test@example.com>
2021-02-02 20:47:08 +0300

    create Router class

--- public/index.php
+++ public/index.php
@@ -1,4 +1,22 @@
 <?php
+require '../system/core/functions.php';
+require '../system/core/Router.php';
+require '../app/controllers/MainController.php';
+require '../app/controllers/PageController.php';
 
-echo '<pre>';
-print_r($_REQUEST);
+use system\core\Router;
+
+$urlStr = $_SERVER['QUERY_STRING'];
+
+//Router::add(['news' => ['controller' => 'news', 'action' => 'index']]);
+//Router::add(['page/contact' => ['controller' => 'page', 'action' => 'contact']]);
+//Router::add(['news/view' => ['controller' => 'news', 'action' => 'view']]);
+Router::add(['^$' => ['controller' => 'Main', 'action' => 'index']]);
+Router::add(['^(?P<controller>[a-z0-9-]+)/?(?P<action>[a-z0-9-]+)?$' => []]);
+
+
+pr(Router::$routers);
+
+Router::dispatch($urlStr);
+
+//pr(Router::$route);
--- system/core/Router.php
+++ system/core/Router.php
@@ -7,24 +7,51 @@ namespace system\core;
 class Router
 {
     public static $routers = [];
-    public static $rout;
-
-    public function __construct()
-    {
-        echo 'Router';
-    }
+    public static $route = [];
 
 
     /**
-     * метод получает строку (путь) и записывает его в таблицу (массив) маршрутов
-     * @param $pattern
-     * @param $path
+     * метод принимает путь (строку) и записывает в таблицу маршрутов (массив)
+     * @param $route
      */
-    public static function getRouts($pattern, $path)
+    public static function add($route)
     {
-        self::$routers[$pattern] = $path;
-        pr(self::$routers);
+        foreach ($route as $key => $val){
+            self::$routers[$key] = $val;
+        }
     }
+
+    /**
+     * метод проверяет url на совпадение с ключом в таблице маршрутов и записывает совпадение в массив rout. и возвращает true или false
+     * @param $url
+     * @return bool
+     */
+    public static function checkRoute($url)
+    {
+        foreach (self::$routers as $key => $val){
+            if (preg_match("#$key#i", $url, $matches)) {
+                $rout = $val;
+
+                foreach ($matches as $k => $match){
+                    if(is_string($k)){
+                       $rout[$k] = $match;
+                   }
+               }
+
+               $rout['controller'] = self::upperStr($rout['controller']);
+
+               if(!isset($route['action'])){
+                   $rout['action'] = 'index';
+               }
+                self::$route = $rout;
+
+               return true;
+            }
+        }
+
+        return false;
+    }
+
 
     /**
      * метод принимает строку (путь) и определяет находится этот путь в таблице (массиве) маршрутов
@@ -32,8 +59,32 @@ class Router
      */
     public static function dispatch($path)
     {
-        if(array_key_exists($path, self::$routers)) {
-            $path = explode('/', );
+        if (self::checkRoute($path)){
+            $controller = 'app\controllers\\' . self::$route['controller'] . "Controller";
+
+            if (class_exists($controller)){
+                $obj = new $controller;
+            }
+            else {
+                echo "Контроллер $controller не найден";
+            }
         }
+        else {
+            echo '404';
+        }
+    }
+
+    private static function upperStr($str)
+    {
+        $str = str_replace("-", " ", $str);
+        $str = ucwords($str);
+        $str = str_replace(" ", "", $str);
+
+        return $str;
+    }
+
+    private static function lowerStr()
+    {
+
     }
 }
